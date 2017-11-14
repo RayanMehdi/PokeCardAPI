5211005f2136f69e888dbac35fc3e01a1c4bbdad
RayanMehdi <rayanmehdi1@gmail.com>
2017-11-14 15:01:25 +0100

    Ajout de json en dur pour tester la liste dans l'appli

--- src/Pokemon/Controller/IndexController.php
+++ src/Pokemon/Controller/IndexController.php
@@ -2,15 +2,24 @@
 
 namespace App\Pokemon\Controller;
 
+use App\Pokemon\Entity\Pokemon;
+
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
+
 
 class IndexController
 {
     public function listAction(Request $request, Application $app)
     {
-      $content = json_encode ('Content');
+
+      $pok = new Pokemon("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png", "bulbizarre", 50, 50, 50, 50, 50, 50);
+      $pok2= new Pokemon("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png", "dracofeu", 100, 150, 500, 555, 785, 44);
+      $pok3= new Pokemon("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/33.png", "nidorino", 66, 48, 20, 45, 77, 88);
+      $pok4= new Pokemon("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/125.png", "elektek", 77, 56, 778, 456, 26, 560);
+      $content =json_encode(array(json_decode($pok->toJson(), true),json_decode($pok2->toJson(), true),json_decode($pok3->toJson(),true),json_decode($pok4->toJson(),true)));
       return new Response($content, Response::HTTP_OK, array('content-type' => 'application/json'));
 
     }
--- src/Pokemon/Entity/Pokemon.php
+++ src/Pokemon/Entity/Pokemon.php
@@ -2,13 +2,39 @@
 
 namespace App\Pokemon\Entity;
 
-class User
+class Pokemon
 {
-    protected $id;
+  protected $icon;
+  protected $name;
+  protected $pv;
+  protected $att;
+  protected $def;
+  protected $spA;
+  protected $spD;
+  protected $spe;
 
-    protected $nom;
 
-    protected $prenom;
+  public  function __construct($icon, $name, $pv, $att, $def, $spA, $spD, $spe){
+    $this->icon = $icon;
+    $this->name = $name;
+    $this->att = $att;
+    $this->def = $def;
+    $this->spA = $spA;
+    $this->pv = $pv;
+    $this->spD = $spD;
+    $this->spe = $spe;
+  }
 
+  public    function toJson(){
+    return json_encode( array(
+        'icon' => $this->icon,
+        'name' => $this->name,
+        'pv' => $this->pv,
+        'att' => $this->att,
+        'def' => $this->def,
+        'spA' => $this->spA,
+        'spD' => $this->spD,
+        'spe' => $this->spe));
+  }
 
 }
